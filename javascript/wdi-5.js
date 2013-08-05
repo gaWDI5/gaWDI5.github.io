d6b040d742b529eac21d109634dff3473d195f38
amadden80 <amadden80@gmail.com>
2013-08-04 22:05:41 -0400

    Image Growing

--- javascript/wdi-5.js
+++ javascript/wdi-5.js
@@ -7,24 +7,41 @@ function focus_on_member(mem_inx){
     if (current_member){
         $('#focus-team-member').empty()
 
-        member_node = $('<div data-id= ' + mem_inx + ' id="' + current_member.lastname + '" class="focus-member sixteen columns" ></div>')
-        member_node.append($('<div>').html('<h2 class="five columns">'+current_member.name+'<h2>'))
+        var member_node = $('<div data-id= ' + mem_inx + ' id="' + current_member.lastname + '" class="focus-member sixteen columns" ></div>')
+        member_node.append($('<div>').html('<h2>'+current_member.name+'<h2>'))
         member_node.append($('<div class="row"></div>'))
-        member_node.append($('<img id="focus-member-image" width="100%" class="seven columns"></img>').attr('src', current_member.images.big_image))
+        var member_node_left = $('<div id="focus-member-left" class="seven columns"></div>')
+        member_node_left.append($('<img id="focus-member-image" class="seven columns" width="100%""></img>').attr('src', current_member.images.big_image))
         
-        info_contact_section = $('<section class="shadowable four columns"></section>')
+        var member_node_right = $('<div id="focus-member-right" class="five columns"></div>')
+        var info_contact_section = $('<section class="shadowable"></section>')
         info_contact_section.html(current_member.bio + '<br>')
         info_contact_section.html(info_contact_section.html() + current_member.email)
-        member_node.append(info_contact_section)
+        member_node_right.append(info_contact_section)
 
-        url_section = $('<section class="shadowable four columns"></section>')
+        member_node_right.append($('<div class="row"></div>'))
+        var url_section = $('<section class="shadowable four columns"></section>')
         url_section.text(current_member.urls.personal_url)
-        member_node.append(url_section)
+        member_node_right.append(url_section)
+
+        member_node.append(member_node_left)
+        member_node.append(member_node_right)
 
         $('#focus-team-member').append(member_node)
         $('#focus-team-member').css('opacity', '1')
     }
 }
+
+function toggle_focus_image_size(){
+    var focus_image = $("#focus-member-image")
+    var toggle_size = "95%"
+
+    focus_image.animate({
+        width: toggle_size,
+    }, 1500 );
+}
+
+
 
 function click_focus_on_member(e){
     var mem_inx = $("#"+ e.target.parentElement.id).data()['id']
@@ -34,23 +51,20 @@ function click_focus_on_member(e){
 
 function load_focus_on_member_pictures(){
 
-    $('#focus-team-member').unbind()
+    $('#focus-team-member').unbind('mouseenter', load_focus_on_member_pictures)
+    $('#team-members-bin').unbind('mouseenter', load_focus_on_member_pictures)
 
-    console.log('Loading all team member pictures...')
     for(var mem=0; mem < team_data.length; mem++) {
         $('#hiden-focus-team-member').empty()
         var current_member_prep = team_data[mem]
         $('#hiden-focus-team-member').append($('<img width="0px" height="0px"></img>').attr('src', current_member_prep.images.big_image))
     }
-    console.log(':)')
-
 
 }
 
 
 // Document Ready Yo!
 $(function(){
-
 
     for(var mem=0; mem < team_data.length; mem++) {
         // Collect Member
@@ -67,13 +81,13 @@ $(function(){
 
     $('#team-members-bin').on('click', '.team-member', click_focus_on_member)
 
-
-
     focus_on_member(Math.floor(Math.random()*team_data.length))
     // $('#focus-team-member').css('opacity', '0.5')
 
     $('#focus-team-member').on('mouseenter', load_focus_on_member_pictures)
+    $('#team-members-bin').on('mouseenter', load_focus_on_member_pictures)
 
+    $('#focus-team-member').on('click', toggle_focus_image_size)
 
     $('#image-box').on('mouseenter', function(){
         $('#group-photo').attr('src', './pageContent/images/Group2.jpg')

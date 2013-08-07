03d4d206b0e598644a0fe9b2f54e36071bfcf871
amadden80 <amadden80@gmail.com>
2013-08-07 16:58:04 -0400

    Add Some Team Data

--- javascript/team.js
+++ javascript/team.js
@@ -57,29 +57,38 @@ var team_data = [
     new Team_Member(
         "Alex Davidow",
         "alex.davidow@gmail.com",
-        "A WDI_5 Student",
+        "I am a member of WDI-5.",
         new Images("./pageContent/images/team/AlexDavidow_big.png"),
-        new URLS("http://wdi5.com",
+        new URLS("http://alexdavidow.github.io",
                  "http://github.com/alexshotime",
                  "http://twitter.com/alexshotime"
-                 )     
+                 ),
+        [
+            new Work_Item('Once-Upon-a-Cupcake', "http://once-upon-a-cupcake.herokuapp.com/", "./pageContent/images/work_screenshots/OnceUponACupcake.jpg"),
+            new Work_Item('Cupcake Game', "http://cupcake-game.herokuapp.com/", "./pageContent/images/work_screenshots/CupcakeGame.jpg"),
+            new Work_Item('World Cup 2014', "http://worldcup-app.herokuapp.com/", "./pageContent/images/work_screenshots/WorldCup2014.jpg")
+        ]      
         ),
 
     new Team_Member(
         "David Aaron",
         "davidmatthewaaron@gmail.com",
-        "A WDI_5 Student",
+        "I am a member of WDI-5.",
         new Images("./pageContent/images/team/DavidAaron_big.png"),
-        new URLS("http://wdi5.com",
+        new URLS("http://dmaaron.github.io/",
                  "http://github.com/dmaaron",
                  "http://twitter.com/davidsonpoint" 
-                 )     
+                 ),
+        [
+            new Work_Item('Stoopendous', "http://stoopendous.herokuapp.com/", "./pageContent/images/work_screenshots/Stoopendous.jpg"),
+            new Work_Item('Fish Friendzy', "http://fish-friendzy.herokuapp.com", "./pageContent/images/work_screenshots/FishFriendz.jpg")
+        ] 
         ),
 
     new Team_Member(
         "Jamie Yu-Ramos",
         "pccjamie@gmail.com",
-        "A WDI_5 Student",
+        "I am a member of WDI-5.",
         new Images(),
         new URLS("http://wdi5.com",
                  "http://github.com/pccjamie",
@@ -90,18 +99,21 @@ var team_data = [
     new Team_Member(
         "Lee Hampton",
         "wdi5ga2013@gmail.com",
-        "A WDI_5 Student",
+        "I am a member of WDI-5.",
         new Images("./pageContent/images/team/LeeHampton_big.png"),
-        new URLS("http://wdi5.com",
+        new URLS("http://leehampton.github.io/",
                  "http://github.com/LeeHampton",
                  "http://twitter.com/leejhampton" 
-                 )     
+                 ),
+        [
+            new Work_Item('Ace Office', "http://lhtj.herokuapp.com/", "./pageContent/images/work_screenshots/AceOffice.jpg")
+        ]      
         ),
 
     new Team_Member(
         "Michael Frohberg",
         "michaelfrohberg@live.com",
-        "A WDI_5 Student",
+        "I am a member of WDI-5.",
         new Images("./pageContent/images/team/MichaelFrohberg_big.png"),
         new URLS("http://wdi5.com",
                  "http://github.com/mixedmedia",
@@ -113,7 +125,7 @@ var team_data = [
     new Team_Member(
         "Mike Reinhart",
         "mreinhart88@gmail.com",
-        "A WDI_5 Student",
+        "I am a member of WDI-5.",
         new Images("./pageContent/images/team/MichaelReinhart_big.png"),
         new URLS("http://wdi5.com",
                  "http://github.com/mikereinhart",
@@ -124,89 +136,111 @@ var team_data = [
     new Team_Member(
         "George Rodriguez",
         "1@6e0r9e.com",
-        "A WDI_5 Student",
+        "I am a member of WDI-5.",
         new Images(),
         new URLS("http://wdi5.com",
                  "http://github.com/6e0r9e",
                  "http://twitter.com/ale6e0r9exshotime" 
-                 )     
+                 ),
+        [
+            new Work_Item('Blings', "http://Blings.herokuapp.com", "./pageContent/images/work_screenshots/Blings.jpg")
+        ] 
         ),
 
     new Team_Member(
         "Tanay Jalan",
         "tanayjalan@gmail.com",
-        "A WDI_5 Student",
+        "I am a member of WDI-5.",
         new Images("./pageContent/images/team/TannayJalan_big.png"),
-        new URLS("http://wdi5.com",
+        new URLS("http://jalant.github.io/",
                  "http://github.com/jalant",
                  "http://twitter.com/TanayJalan" 
-                 )     
+                 ),
+        [
+            new Work_Item('Stoopendous', "http://stoopendous.herokuapp.com/", "./pageContent/images/work_screenshots/Stoopendous.jpg"),
+            new Work_Item('Ace Office', "http://lhtj.herokuapp.com/", "./pageContent/images/work_screenshots/AceOffice.jpg"),
+        ]    
         ),
 
     new Team_Member(
         "Chris Eichler",
         "chriseichler@gmail.com",
-        "A WDI_5 Student",
+        "I am a member of WDI-5.",
         new Images("./pageContent/images/team/ChrisEichler_big.png"),
         new URLS("http://wdi5.com",
                  "http://github.com/chriseichler",
                  "http://twitter.com/Chris_R_Eichler" 
-                 )     
+                 )   
         ),
 
     new Team_Member(
         "Isabel Cortes",
         "belcortes@gmail.com",
-        "A WDI_5 Student",
+        "I am a member of WDI-5.",
         new Images("./pageContent/images/team/BelCortes_big.png"),
-        new URLS("http://wdi5.com",
+        new URLS("http://belcortes.github.io/",
                  "http://github.com/belcortes",
                  "http://twitter.com/bebelcortes" 
-                 )     
+                 ),
+        [
+            new Work_Item('Calcacademy', "http://calcacademy.herokuapp.com/"),
+            new Work_Item('WDI5.com', "http://WDI5.com", "./pageContent/images/work_screenshots/WDI5.jpg"),
+            new Work_Item('World Cup 2014', "http://worldcup-app.herokuapp.com/", "./pageContent/images/work_screenshots/WorldCup2014.jpg"),
+            new Work_Item('Cupcake Game', "http://fathomless-fjord-8804.herokuapp.com/users/sign_in", "./pageContent/images/work_screenshots/CupcakeGame.jpg")
+        ]  
         ),
 
     new Team_Member(
         "Andrew Madden",
         "amadden80@gmail.com",
         "My business card say I'm an Audio Research, Programmer and Nice Guy.  I need a new business card...",
-        new Images("./pageContent/images/team/AndrewMadden/AndrewMadden_big.png"),
+        new Images("./pageContent/images/team/AndrewMadden_big.png"),
         new URLS("http://AndrewMadden.com",
                  "http://github.com/amadden80",
                  "http://twitter.com/ajm_dev"
                  ),
         [
-            new Work_Item('Sonifzer API', "http://Sonifizer.com", "./pageContent/images/team/AndrewMadden/Sonifizer.jpg"),
-            new Work_Item('WDI5.com', "http://WDI5.com", "./pageContent/images/team/AndrewMadden/WDI5.jpg"),
-            new Work_Item('Blings', "http://Blings.herokuapp.com", "./pageContent/images/team/AndrewMadden/Blings.jpg")
+            new Work_Item('Sonifzer API', "http://Sonifizer.com", "./pageContent/images/work_screenshots/Sonifizer.jpg"),
+            new Work_Item('WDI5.com', "http://WDI5.com", "./pageContent/images/work_screenshots/WDI5.jpg"),
+            new Work_Item('Blings', "http://Blings.herokuapp.com", "./pageContent/images/work_screenshots/Blings.jpg")
         ] 
         ),
 
     new Team_Member(
         "Remy Bartolotta",
         "remy.bartolotta@gmail.com",
-        "A WDI_5 Student",
+        "Full stack Chukanbushi (Middle Samurai) looking for a Shogun (Legendary Samurai and Force Commander) in order to mature my skills.",
         new Images("./pageContent/images/team/RemyBartolotta_big.png"),
-        new URLS("http://wdi5.com",
+        new URLS("http://www.WhoIsRemy.com",
                  "http://github.com/bartolotta",
                  "http://twitter.com/WhoIsRemy" 
-                 )     
+                 ),
+        [
+            new Work_Item('Curtopia', "http://curtopia.herokuapp.com", "./pageContent/images/work_screenshots/Curtopia.jpg"),
+            new Work_Item('WifiCiti', "http://wificiti.herokuapp.com", "./pageContent/images/work_screenshots/WifiCiti.jpg")
+        ]    
         ),
 
     new Team_Member(
         "Mike Schechter",
         "schechtermike@gmail.com",
-        "A WDI_5 Student",
+        "Speed Quality Price:  Pick any two.",
         new Images("./pageContent/images/team/MichaelSchechter_big.png"),
-        new URLS("http://wdi5.com",
+        new URLS("http://schechter.github.io/",
                  "http://github.com/schechter",
                  "http://twitter.com/mschechter77" 
-                 )     
+                 ),
+        [
+            new Work_Item('Party Of Five', "http://party-of-five.herokuapp.com/", "./pageContent/images/work_screenshots/PartyOfFive.jpg"),
+            new Work_Item('Fish Friendzy', "http://fish-friendzy.herokuapp.com", "./pageContent/images/work_screenshots/FishFriendz.jpg"),
+            new Work_Item('WifiCiti', "http://wificiti.herokuapp.com", "./pageContent/images/work_screenshots/WifiCiti.jpg")
+        ]         
         ),
 
     new Team_Member(
         "Bernard Doherty",
         "bernard.doherty@icloud.com",
-        "A WDI_5 Student",
+        "I am a member of WDI-5.",
         new Images("./pageContent/images/team/BernardDoherty_big.png"),
         new URLS("http://wdi5.com",
                  "http://github.com/BernardDoherty",
@@ -217,7 +251,7 @@ var team_data = [
     new Team_Member(
         "Ryan Kuhel",
         "wdi5ga2013@gmail.com",
-        "A WDI_5 Student",
+        "I am a member of WDI-5.",
         new Images(),
         new URLS("http://wdi5.com",
                  "http://github.com/rkuhel",
@@ -233,7 +267,7 @@ var team_data = [
         new URLS("http://wdi5.com",
                  "http://github.com/FJMendez",
                  "http://twitter.com/Abrasive_Fellow" 
-                 )     
+                 ) 
         )
 ];   
 
